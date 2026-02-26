8648fffe3d036b5836cc7567ca810cf039784cdd
uchihadeann <uchihadeann@gmail.com>
2026-02-26 22:04:39 +0800

    leetcode 2

--- Main.java
+++ Main.java
@@ -1,12 +1,19 @@
 class Solution {
-    public int mySqrt(int x) {
-        if(x == 0) return 0;
+    public int[] plusOne(int[] digits) {
 
-        long res = x;
+        for (int i = digits.length - 1; i >= 0; i--) {
 
-        while(res > x / res){
-            res = (res + x / res) / 2;
+            if (digits[i] != 9) {
+                digits[i]++;
+                return digits;
+            }
+
+            digits[i] = 0;
         }
-        return (int)res;
+
+        
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+        return result;
     }
 }
